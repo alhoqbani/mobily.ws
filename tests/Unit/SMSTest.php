ab9c66b19027fd198627f5e0878dfb5a5ae7eccc
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-04-20 09:32:00 -0700

    Changes test structue

--- tests/Unit/SMSTest.php
+++ tests/Unit/SMSTest.php
@@ -14,9 +14,23 @@ class SMSTest extends TestMobilyWs
         $handler = $this->createResponse('2300/6000');
         $sms = new SMS($handler);
         $balance = $sms->balance();
-        
+
         $this->assertEquals(6000, $balance);
         $this->assertInternalType('int', $balance);
     }
+    
+    /** @test */
+    public function it_accepts_single_multiple_phone_numbers() {
+        $sms =new SMS();
+        $sms->to('966000000001');
+        $sms->to(['966000000002', '966000000003']);
+        
+        $this->assertCount(3,
+            $sms->to(),
+        'The registered phone numbers by to() should be 3'
+        );
+    }
+    
+    
     
 }
